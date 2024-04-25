e87e4d6074c80feff77e5b62e4c06588a76b403a
sparsh mathur <mathursparsh01@gmail.com>
2024-04-25 22:26:46 +0530

    prod server constant

--- Constant.js
+++ Constant.js
@@ -1,2 +1,4 @@
 // export const baseurl = "http://192.168.0.24:5000/api";
-export const baseurl = "https://diamond-trade.onrender.com/api";
+// export const baseurl = "https://diamond-trade.onrender.com/api";
+export const baseurl =
+  "http://ec2-15-206-66-35.ap-south-1.compute.amazonaws.com/api";
